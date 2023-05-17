d0eccdb9de621691469ca3840c88f3fe195c1209
Abduvokhidjon <aabduvohid33@gmail.com>
2023-05-17 13:05:15 -0500

    first commit

---
+++ test_python.py
@@ -0,0 +1,32 @@
+message=('Hello World')
+# letter = message.replace('World', 'Universe')
+# ali = letter.replace('Hello', "What's up")
+# shak = ali.replace('chotam', 'how you doing?') 
+# print(message. lower())
+# print(mes sage.count("o"))
+# print(message.find ('W') )        
+greeting = 'Hello'
+name = 'Ali' 
+intro = ' Welcome!' 
+# # message = greeting + ', ' + name + intro
+# # print(message)
+# # message = '{}, {} {}'. format(greeting, name, intro)
+message = f'{greeting}, {name.upper()} {intro}'
+
+print(message)
+
+
+
+# name = input("What's your name? ")
+# print("Hello, " + name + "! Nice to meet you.")
+# greeting = input ("How are you? ")
+
+# banned_words = ["fuck", "fuCK", "latta"]
+
+# if greeting in banned_words:
+#     greeting = "chichqoq"
+#     print("Hello, " + greeting + "! Nice to meet you.")
+# else:
+#     print("Hello, " + greeting + "! Nice to meet you.")
+
+
